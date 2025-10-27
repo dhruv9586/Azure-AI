8c48b803fcf5ead3bd7a82bda246aee82cd80636
dhruv9586 <dhruv9586@gmail.com>
2025-10-27 12:50:47 +0530

    update doc url to none for safety

--- src/document_intelligence/anaysis.py
+++ src/document_intelligence/anaysis.py
@@ -1,11 +1,14 @@
 from client import client
 from azure.ai.documentintelligence.models import AnalyzeDocumentRequest, AnalyzeResult
 
+# Enter actual file url to analyze
+document_url = ""
+
 # Azure Blob document URL
 # response = client.begin_analyze_document(
 #     "prebuilt-read",
 #     AnalyzeDocumentRequest(
-#         url_source="https://delstorage5000.blob.core.windows.net/documents/receipt.png"
+#         url_source=document_url
 #     ),
 # )
 
@@ -20,9 +23,7 @@ from azure.ai.documentintelligence.models import AnalyzeDocumentRequest, Analyze
 # Azure Blob document URL
 response = client.begin_analyze_document(
     "prebuilt-invoice",
-    AnalyzeDocumentRequest(
-        url_source="https://delstorage5000.blob.core.windows.net/documents/receipt.png?sp=r&st=2025-10-26T05:52:49Z&se=2025-10-26T14:07:49Z&spr=https&sv=2024-11-04&sr=b&sig=u1x%2FB7QmLxAd30jzoivmsL4YDWwkeXti8iUoBw%2FRCRA%3D"
-    ),
+    AnalyzeDocumentRequest(url_source=document_url),
 )
 
 result = response.result()
